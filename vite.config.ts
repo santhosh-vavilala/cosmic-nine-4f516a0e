23e653815d1dd3f3d0e4d384ce6b945b1dc9c13e
Santhosh Vavilala <santhosh.vavilala@hotmail.com>
2026-01-25 12:30:13 +0530

    changes for base path

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/cosmic-nine-4f516a0e/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
