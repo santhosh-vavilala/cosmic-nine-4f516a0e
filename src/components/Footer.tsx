89085e2d9e9e99db8238667a4366820545602f9e
Santhosh Vavilala <santhosh.vavilala@hotmail.com>
2026-01-28 17:46:10 +0530

    add home page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,10 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 z-[999]">
       <div className="container mx-auto text-center space-x-4">
+        <Link to="/" className="hover:underline">
+          Home
+        </Link>
+        <span>|</span>
         <Link to="/policies?section=privacy-policy" className="hover:underline">
           Privacy Policy
         </Link>
